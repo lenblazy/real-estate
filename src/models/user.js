1e87e5aa68edff5f8c096d0cc7cf714a3b5daa42
Lennox Mwabonje <lenblazy@gmail.com>
2025-02-22 14:10:58 +0300

    Add user model

---
+++ src/models/user.js
@@ -0,0 +1,69 @@
+import mongoose from 'mongoose';
+
+const {Schema, ObjectId, model} = mongoose;
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            minLength: 5,
+            lowercase: true,
+        },
+        name: {
+            type: String,
+            required: true,
+            default: "",
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+            index: true,
+        },
+        address: {
+            type: String,
+            trim: true,
+            default: "",
+        },
+        phone: {
+            type: String,
+            trim: true,
+            default: "",
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 6,
+            max: 64,
+        },
+        role: {
+            type: [String],
+            default: ["Buyer"],
+            enum: ["Seller","Buyer","Admin","Author"],
+        },
+        photo: {},
+        logo: {},
+        company: {
+            type: String,
+            default: "",
+        },
+        enquiredProperties: [
+            { type: ObjectId, ref: 'Ad'}
+        ],
+        wishList: [
+            { type: ObjectId, ref: 'Ad'}
+        ],
+        about: {
+            type: String,
+            default: "",
+        }
+    },
+    {timestamps: true}
+);
+
+export default model('User', userSchema);
