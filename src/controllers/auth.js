65c6c22416bfd51e4f14eba49e829fad623e4564
Lennox Mwabonje <lenblazy@gmail.com>
2025-02-22 14:16:38 +0300

    Add Login validation checks

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,7 +1,31 @@
+import validator from "email-validator";
+
 export const login = (req, res) => {
-    res.json({
-        ...req.body,
-        message: "Logged in successfully"
-    })
+
+    const { email, password } = req.body;
+
+    // validations
+    if (!validator.validate(email)) {
+        return res.json({ error: "A valid Email is required" });
+    }
+
+    if (!email?.trim()) {
+        return res.json({ error: "Email is required" });
+    }
+
+    if (!password.trim()) {
+        return res.json({ error: "Password is required" });
+    }
+
+    if (password.length < 6) {
+        return res.json({ error: "Password must be at least 6 characters" });
+    }
+
+    try {
+
+    } catch (e) {
+
+    }
+
 }
 
